8e39a5ca456d306c8b74af5777247088a55a014b
Morchid <youssef.morchid@grenoble-inp.org>
2026-01-28 12:35:40 +0100

    add preprocessing functions

--- src/inference/realtime_sensor.py
+++ src/inference/realtime_sensor.py
@@ -46,7 +46,54 @@ def save_alert(conn, src_ip, dst_ip, protocol, confidence):
    )
    conn.commit()
 
-conn = init_database(db_path="data/ids_events.db")
-save_alert(conn, '0','0', '0', 0.1)
-  
+# 2. PREPROCESSING FUNCTIONS
+# Load scaler, encoder, and feature list
+def load_artifacts(artifacts_dir="models/artifacts"):
+  artifacts_dir = Path(artifacts_dir)
+  with open(artifacts_dir / "selected_features.json", 'r') as f:
+        features = json.load(f)
+  scaler = joblib.load(artifacts_dir / "minmax_scaler.pkl")    
+  encoder = joblib.load(artifacts_dir / "protocol_encoder.pkl")
 
+  print(f"Loaded {len(features)} features")
+  return features, scaler, encoder
+
+# Transform raw flow into model-ready vector
+def preprocess_flow(flow, features, scaler, encoder):
+    values = []
+
+    for feature_name in features:
+        if feature_name == 'protocol':
+            protocol = flow.get('protocol', 'UNKNOWN')
+
+            # Map ICSFlowGenerator format to training format
+            protocol_map = {
+                'IP:TCP': 'IPV4-TCP',
+                'IP:UDP': 'IPV4-UDP',
+                'IP:ICMP': 'IPV4-ICMP' 
+            }
+            protocol = protocol_map.get(protocol,protocol)
+
+            try:
+                encoded = encoder.transform([protocol])[0]
+            except:
+                encoded = 2 # Default
+            values.append(encoded)
+
+        else:
+            val = flow.get(feature_name, 0)
+            if pd.isna(val):
+                val = 0
+            values.append(float(val))
+    
+    # Normalize
+    vector = np.array(values).reshape(1, -1)
+    normalized = scaler.transform(vector)
+    return normalized
+
+
+flow = {'protocol': 'IP:TCP', 'rBytesAvg': 90.5}
+features, scaler, encoder = load_artifacts(artifacts_dir="models/artifacts")
+normalized = preprocess_flow(flow, features, scaler, encoder)
+print(normalized)
+print(encoder.classes_)
